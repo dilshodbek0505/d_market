31f6a9e8c536340ca71b033fcc539b20b81218ef
dilshodbek0505 <ddilshod0415@fmail.com>
2024-09-21 02:16:48 +0500

    settings are done

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -5,7 +5,6 @@ from apps.common import models
 
 @admin.register(models.VersionHistory)
 class VersionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "version", "required", "created_at", "updated_at")
-    list_display_links = ("id", "version")
+    list_display = ("version", "required", "created_at", "updated_at")
     list_filter = ("required", "created_at", "updated_at")
     search_fields = ("version",)
